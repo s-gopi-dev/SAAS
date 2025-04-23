d79188d7c7bcd3ae7c10ac4d565dc108cdd56ab3
S Gopi <sgopi.dev.mail@gmail.com>
2025-04-23 18:06:17 +0530

    2.55.8 custom django managemnt command to pull vendors

--- src/saas/settings.py
+++ src/saas/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'visits',  # Custom app for tracking page visits
+    'custom_command',  # Custom command app
 ]
 
 MIDDLEWARE = [
@@ -143,6 +144,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_BASE_DIR = BASE_DIR / "staticFiles"
+STATICFILES_BASE_DIR.mkdir(parents=True, exist_ok=True)
+STATICFILES_VENDOR_DIR = STATICFILES_BASE_DIR / "vendors"
+
+
+# source(s) for python manage.py collectstatic
+STATICFILES_DIRS = [
+    STATICFILES_BASE_DIR,
+]
+
+# output for the python manage.py collectstatic 
+# local cdn
+
+STATIC_ROOT = BASE_DIR.parent / "local-cdn"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
